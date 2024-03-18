e34df9fce5c34ae9add9eb3f86857b96390b345e
Franklin Z <franklin.zhunioiniguez@gmail.com>
2024-03-18 01:46:34 -0500

    graficacion Series de tiempo xts

--- FA_Tipos_Datos_Finanzas.R
+++ FA_Tipos_Datos_Finanzas.R
@@ -21,3 +21,19 @@ precios_volumenes("AMZN")
 precios_volumenes("NFLX")
 precios_volumenes("IBM")
 precios_volumenes("SPY")
+
+
+PyV <- merge.xts(AMZN, NFLX, IBM, SPY)
+colnames(PyV) <- c("Amazon P.Cierre", "Amazon Vol", "Netflix P.Cierre", "Netflix Vol",
+                   "IBM P.Cierre", "IBM Vol", "SP500 P.Cierre", "SP500 Vol")
+
+
+Precios <- dygraph(PyV[,c(1,3,5,7)], main = "Precios de Amazon, Netflix, IMB y SP&500") %>%
+  dyAxis("y", label = "Precios") %>%
+  dyRangeSelector(dateWindow = c("2014-01-01", "2020-07-01"))%>%
+  dyOptions(colors = RColorBrewer::brewer.pal(4, "Set1"))
+
+Precios
+
+
+
